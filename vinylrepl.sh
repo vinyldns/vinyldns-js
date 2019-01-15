d1d7e4c433bc17e0c3a84e66333a64bfc249980f
Mike Ball <mikedball@gmail.com>
2019-01-14 21:49:08 -0500

    remove vinylrepl.sh for now

--- vinylrepl.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$( cd $(dirname $0) ; pwd -P )
-node -i -e "var vinyl = require('$DIR/build/vinyldns.bundle.js'); \
-            var vinylClient = new vinyl.VinylDns(true);"
