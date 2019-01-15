108b3bbda49c2a1ee85676d11cffd867ad530159
Mike Ball <mikedball@gmail.com>
2019-01-14 21:47:44 -0500

    add basic test

---
+++ test/fixtures/responses.js
@@ -0,0 +1,17 @@
+module.exports = {
+  getZones: {
+    "zones": [{
+      "status": "Active",
+      "account": "a0b5ea74-cc05-4932-a294-9bf935d52744",
+      "name": "list-zones-test-searched-1.",
+      "created": "2016-12-16T15:21:47Z",
+      "adminGroupId": "a0b5ea74-cc05-4932-a294-9bf935d52744",
+      "email": "test@test.com",
+      "shared": false,
+      "acl": {
+        "rules": []
+      },
+      "id": "31a3d8a9-bea0-458f-9c24-3d39d4b929d6"
+    }]
+  }
+};
---
+++ test/vinyldns_test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const nock = require('nock');
+const fixtures = require('./fixtures/responses');
+const VinylDns = require('../src/vinyldns');
+
+const host = 'http://my-vinyldns.com';
+const vinyl = new VinylDns({
+  apiUrl: host,
+  accessKeyId: '123',
+  secretAccessKey: '123'
+});
+
+function mockGet(path, resp) {
+  return nock(host)
+    .get(path)
+    .reply(200, resp);
+}
+
+
+describe('VinylDns', () => {
+  it('is configurable', () => {
+    assert.equal(vinyl.config.apiUrl, 'http://my-vinyldns.com');
+  });
+
+  describe('getZones', () => {
+    it('fetches zones', (done) => {
+      mockGet('/zones', fixtures.getZones);
+
+      vinyl.getZones()
+        .then(result => {
+          assert.equal(result.zones[0].name, 'list-zones-test-searched-1.');
+
+          done();
+        });
+    });
+  });
+});
