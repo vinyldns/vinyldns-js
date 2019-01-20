88f99d5739e8e9ac9960b03140b7f63a1adba2d3
Mike Ball <mikedball@gmail.com>
2019-01-20 13:00:02 -0500

    ensure integration test script failure

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+set -e
+
+echo "Running vinyldns-js integration tests..."
+
 if [ ! -d ".vinyldns" ]; then \
-  echo "fetching VinylDNS source code..."; \
+  echo "fetching VinylDNS source code..."
   git clone https://github.com/vinyldns/vinyldns .vinyldns; \
 fi
 
+echo "Starting localhost:9000 VinylDNS API instance..."
 .vinyldns/bin/docker-up-vinyldns.sh \
   --api-only \
   --version 0.8.0
+
+echo "Executing vinyldns-js integration tests..."
 
 node_modules/.bin/mocha test/integration/*.js
 
