03b5574a39bc13fe60e2c9abdae75acd81d32883
Mike Ball <mikedball@gmail.com>
2019-02-13 10:33:27 -0500

    WIP: add vinyldns docs

--- src/vinyldns.js
+++ src/vinyldns.js
@@ -19,44 +19,106 @@ const aws4 = require('aws4');
 const url = require('url');
 const Urls = require('./urls');
 
+/**
+ * Represents VinylDNS API.
+ * @class
+ */
 class VinylDNS {
+
+  /**
+   * Instantiate a VinylDNS instance.
+   * @constructor
+   * @param {object} config - The configuration object.
+   * @param {string} config.apiURL - The VinylDNS API URL.
+   * @param {string} config.accessKeyId - The VinylDNS API access key ID.
+   * @param {string} config.secretAccessKey - The VinylDNS API secret access key.
+   */
   constructor(config) {
     this.config = config;
     this.urls = new Urls(config.apiUrl);
   }
 
+  /**
+   * Fetch zones.
+   * @param {object} queryOpts - The zone query parameters.
+   * @param {string} queryOpts.nameFilter - One or more characters contained in the name of a zone to search for.
+   * @param {number} queryOpts.startFrom - In order to advance through pages of results, the startFrom is set to the nextId that is returned on the previous response.
+   * @param {number} queryOpts.maxItems - The number of items to return in the page. Valid values are 1 - 100.
+   */
   getZones(queryOpts) {
     return this._getOrDelete(this.urls.getZones(queryOpts), 'get');
   }
 
+  /**
+   * Fetch zone.
+   * @param {string} id - The zone ID.
+   */
   getZone(id) {
     return this._getOrDelete(this.urls.zone(id), 'get');
   }
 
+  /**
+   * Fetch zone changes.
+   * @param {string} id - The zone ID.
+   * @param {object} queryOpts - The zone changes query parameters.
+   * @param {number} queryOpts.startFrom - In order to advance through pages of results, the startFrom is set to the nextId that is returned on the previous response.
+   * @param {number} queryOpts.maxItems - The number of items to return in the page. Valid values are 1 - 100.
+   */
   getZoneChanges(id, queryOpts) {
     return this._getOrDelete(this.urls.getZoneChanges(id, queryOpts), 'get');
   }
 
+  /**
+   * Sync zone.
+   * @param {string} id - The zone ID.
+   */
   syncZone(id) {
     return this._sync(this.urls.syncZone(id));
   }
 
+  /**
+   * Create zone.
+   * @param {object} zone - The VinylDNS zone to create. See the [VinylDNS zone docs]{@link https://www.vinyldns.io/api/create-zone.html} to learn more.
+   */
   createZone(zone) {
     return this._createOrUpdate(zone, this.urls.zonesBase(), 'post');
   }
 
+  /**
+   * Update zone.
+   * @param {object} zone - The VinylDNS zone to update. See the [VinylDNS zone docs]{@link https://www.vinyldns.io/api/update-zone.html} to learn more.
+   */
   updateZone(zone) {
     return this._createOrUpdate(zone, this.urls.zone(zone.id), 'put');
   }
 
+  /**
+   * Delete zone.
+   * @param {string} id - The zone ID.
+   */
   deleteZone(id) {
     return this._getOrDelete(this.urls.zone(id), 'delete');
   }
 
+  /**
+   * Fetch record sets.
+   * @param {string} zoneId - The ID of the zone in which to query.
+   * @param {object} queryOpts - The record sets query parameters.
+   * @param {string} queryOpts.nameFilter - One or more characters contained in the name of a record set to search for.
+   * @param {number} queryOpts.startFrom - In order to advance through pages of results, the startFrom is set to the nextId that is returned on the previous response.
+   * @param {number} queryOpts.maxItems - The number of items to return in the page. Valid values are 1 - 100.
+   */
   getRecordSets(zoneId, queryOpts) {
     return this._getOrDelete(this.urls.getRecordSets(zoneId, queryOpts), 'get');
   }
 
+  /**
+   * Fetch record set.
+   * @param {object} details - The record set details.
+   * @param {string} queryOpts.nameFilter - One or more characters contained in the name of a record set to search for.
+   * @param {number} queryOpts.startFrom - In order to advance through pages of results, the startFrom is set to the nextId that is returned on the previous response.
+   * @param {number} queryOpts.maxItems - The number of items to return in the page. Valid values are 1 - 100.
+   */
   getRecordSet(details) {
     return this._getOrDelete(this.urls.recordSet(details), 'get');
   }
