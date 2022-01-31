2a856129fcfe46924209933618672c73075400e8
Jay07GIT <jeyraj931@gmail.com>
2022-01-31 17:21:00 +0530

    Added github workflows for release and tests

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-vinyldns_version="0.9.7"
+vinyldns_version="0.10.4"
 
 echo "Running vinyldns-js integration tests..."
 
@@ -11,15 +11,13 @@ if [ ! -d ".vinyldns" ]; then \
   git clone https://github.com/vinyldns/vinyldns --branch "v${vinyldns_version}" .vinyldns; \
 fi
 
-.vinyldns/bin/remove-vinyl-containers.sh
+.vinyldns/utils/clean-vinyldns-containers.sh
 
 echo "Starting localhost:9000 VinylDNS API instance..."
-.vinyldns/bin/docker-up-vinyldns.sh \
-  --api-only \
-  --version "${vinyldns_version}"
+.vinyldns/quickstart/quickstart-vinyldns.sh --api-only 
 
 echo "Executing vinyldns-js integration tests..."
 
 node_modules/.bin/mocha --timeout 3000 test/integration/*.js
 
-.vinyldns/bin/remove-vinyl-containers.sh
+.vinyldns/utils/clean-vinyldns-containers.sh
