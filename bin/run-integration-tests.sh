59a9bc766ec30541969f39f6845a28975a613da6
Jay07GIT <jeyraj931@gmail.com>
2022-02-01 17:46:10 +0530

    Added Makefile for tests and updated the github workflow

--- bin/run-integration-tests.sh
+++ bin/run-integration-tests.sh
@@ -2,22 +2,32 @@
 
 set -e
 
-vinyldns_version="0.10.4"
+VINYLDNS_VERSION="0.10.4"
+
+echo "Removing VinylDNS API if exists.."
+if docker ps | grep -q "vinyldns-js-api"; then
+	 docker kill vinyldns-js-api &> /dev/null || true
+	 docker rm vinyldns-js-api &> /dev/null || true
+   echo "Removed VinylDNS API"
+else echo "VinylDNS API does not exists"
+fi
 
 echo "Running vinyldns-js integration tests..."
 
-if [ ! -d ".vinyldns" ]; then \
-  echo "fetching VinylDNS source code..."
-  git clone https://github.com/vinyldns/vinyldns --branch "v${vinyldns_version}" .vinyldns; \
-fi
+echo "Starting VinylDNS API.."
+docker run -d --name vinyldns-js-api -p "9000:9000" -p "19001:19001" -e RUN_SERVICES="all tail-logs" vinyldns/build:base-test-integration-v$VINYLDNS_VERSION
 
-.vinyldns/utils/clean-vinyldns-containers.sh
-
-echo "Starting localhost:9000 VinylDNS API instance..."
-.vinyldns/quickstart/quickstart-vinyldns.sh --api-only 
+echo "Waiting for VinylDNS API to start.."
+{ timeout "20s" grep -q 'STARTED SUCCESSFULLY' <(timeout 20s docker logs -f vinyldns-js-api);	echo "VinylDNS API STARTED SUCCESSFULLY";  } || { echo "VinylDNS API failed to start"; exit 1; }
 
 echo "Executing vinyldns-js integration tests..."
+npm install && node_modules/.bin/mocha --timeout 3000 test/integration/*.js
 
-node_modules/.bin/mocha --timeout 3000 test/integration/*.js
+echo "Removing VinylDNS API.."
 
-.vinyldns/utils/clean-vinyldns-containers.sh
+if docker ps | grep -q "vinyldns-js-api"; then
+	 docker kill vinyldns-js-api &> /dev/null || true
+	 docker rm vinyldns-js-api &> /dev/null || true
+   echo "Removed VinylDNS API"
+else echo "VinylDNS API does not exists"
+fi
